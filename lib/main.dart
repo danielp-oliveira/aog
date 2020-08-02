41a52bf97828070fcf8ad58b4fa3158c9d2e8549
danielp-oliveira <danielp.oliveira@outlook.com>
2020-08-02 10:50:59 -0300

    Calculando o resultado

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
-import 'package:aog/widgets/logo.widget.dart';
-import 'package:aog/widgets/input.widget.dart';
+import 'package:aog/pages/home.page.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,50 +17,5 @@ class MyApp extends StatelessWidget {
       ),
       home: HomePage(),
     );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  var _gasCtrl = new MoneyMaskedTextController();
-  var _alcCtrl = new MoneyMaskedTextController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        body: ListView(
-          children: <Widget>[
-            Logo(),
-            Input(
-              ctrl: _gasCtrl,
-              label: "Gasolina",
-            ),
-            Input(
-              ctrl: _alcCtrl,
-              label: "Álcool",
-            ),
-            Container(
-              margin: EdgeInsets.all(30),
-              height: 60,
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.8),
-                borderRadius: BorderRadius.circular(
-                  60,
-                ),
-              ),
-              child: FlatButton(
-                child: Text(
-                  "CALCULAR",
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 25,
-                    fontFamily: "Big Shoulders Display",
-                  ),
-                ),
-                onPressed: () {},
-              ),
-            ),
-          ],
-        ));
   }
 }
